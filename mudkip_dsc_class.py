10b0e67e0cf0ffe39d6e385edd891d24324f1538
Taemin Lee <72825653+taeminlee0601@users.noreply.github.com>
2023-01-31 00:23:28 -0500

    Update mudkip_dsc_class.py

--- mudkip_dsc_class.py
+++ mudkip_dsc_class.py
@@ -1,4 +1,5 @@
 from datetime import date
+import typing
 
 class TimeTable:
     late_start_days = [date(2023, 2, 15), date(2023, 2, 22), date(2023, 3, 22), date(2023, 3, 29), date(2023, 4, 19), date(2023, 4, 26), date(2023, 5, 24), date(2023, 5, 31)]
@@ -9,6 +10,16 @@ class TimeTable:
         self.p2 = p2
         self.p3 = p3
         self.p4 = p4
+
+    def set_period(self, per_num, period):
+        if per_num == 1:
+            self.p1 = period
+        elif per_num == 2:
+            self.p2 = period
+        elif per_num == 3:
+            self.p3 = period
+        else:
+            self.p4 = period
 
     def __str__(self):
         today = date.today()
@@ -27,7 +38,6 @@ class TimeTable:
             self.late_start_days.pop(i)
 
         if today != self.late_start_days[0]:
-            periods.append(str(self.late_start_days))
             periods.append("9:00 - 10:20  Period 1: **" + self.p1 + "**")
             periods.append("10:25 - 11:40 Period 2: **" + self.p2 + "**")
             periods.append("11:40 - 12:40 **Lunch**")
@@ -51,3 +61,38 @@ class TimeTable:
                 periods.append("2:15 - 3:15  Period 4: **" + self.p3 + "**")
 
         return '\n'.join(periods)
+
+class Periods:
+    def __init__(self, per_num, course, room = 0, teacher = ""):
+        self.per_num = per_num
+        self.course = course
+        self.room = room
+        self.teacher = teacher
+
+    def set_course(self, course):
+        self.course = course
+
+    def set_room(self, room):
+        self.room = room
+
+    def set_teacher(self, teacher):
+        self.teacher = teacher
+
+    def get_num(self):
+        return self.per_num
+
+    def __str__(self):
+        period_info = []
+
+        period_info.append("Period " + str(self.per_num) + ": **" + self.course + "**")
+        
+        if self.room != 0:
+            period_info.append("Room: **" + str(self.room) + "**")
+        
+        if self.teacher == "":
+            return '\n'.join(period_info)
+        else:
+            period_info.append("Teacher: **" + self.teacher + "**")
+
+        return '\n'.join(period_info)
+
